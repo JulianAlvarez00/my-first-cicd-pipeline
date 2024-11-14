0552562b7289caa403ca669640991c0491a860d0
Julian Alvarez <julianalvarez@Julians-MacBook-Air.local>
2024-11-14 17:36:12 -0300

    feat: Añadido frontend básico con HTML y CSS

--- test_app.py
+++ test_app.py
@@ -1,6 +1,12 @@
 from app import app
 
 def test_hello_world():
+    response = app.test_client().get('/api/hello')
+    assert response.status_code == 200
+    assert 'message' in response.get_json()
+    assert '¡Hola Mundo!' in response.get_json()['message']
+
+def test_index_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.get_json()['message'] == 'Hola Mundo!'
+    assert b'Mi Primera Pipeline DevOps' in response.data
